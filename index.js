613c4507a1e27eaecf0462b2f7a9cd9e9da09835
github username <github email address>
2016-11-06 11:28:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much.";
+const addInitialsToHeroes = heroes => heroes.map(hero => Object.assign({}, hero, {
+  initials: hero.firstName.charAt(0) + hero.lastName.charAt(0),
+}));
+
+//^^^NICE AND PURE
+
+//vvvvv DIRTY AND POOR
+function addInitialsToHeroes(heroes) {
+  heroes.forEach(hero => {
+    hero.initials = hero.firstName.charAt(0) + hero.lastName.charAt(0);
+  });
+
+  return heroes;
+}
+
+let heroesWithInitials = addInitialsToHeroes(heroes);
